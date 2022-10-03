91dfd3b44203f20829aa028eab98096720111f4d
bearddude1 <baapteshuva@gmail.com>
2022-10-04 00:25:51 +0300

    fixed issue

--- dals/index.js
+++ dals/index.js
@@ -1,9 +1,9 @@
-import {connect} from "mongoose";
+import mongoose from "mongoose";
 import { config } from "dotenv";
 config()
 export async function connectMongo() {
    try {
-      await connect(process.env.DB_URL);
+      await mongoose.connect(process.env.DB_URL);
 
    } catch (error) {
       throw new Error(error.message)
